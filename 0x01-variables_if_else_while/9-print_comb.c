ad74539685d5057cab4c8f4d765a689fd6fcd98a
MBrian103 <oduorbrian103@gmail.com>
2022-03-14 19:03:51 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,16 +16,15 @@ int d;
 for (d = '0'; d <= '9'; d++)
 
 {
-
 putchar(d);
+
 if (d != '9')
 
 {
 
-putchar(',');																	
-putchar(' ');
-
-}
+putchar(',');
+																				putchar(' ');
+																						}
 
 }
 
