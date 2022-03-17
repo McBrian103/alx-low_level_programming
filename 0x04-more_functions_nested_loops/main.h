00c104a32af0604814d80cf770315d89233e025d
MBrian103 <oduorbrian103@gmail.com>
2022-03-17 14:49:53 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
 int _putchar(char c);
 int _supper(int c)
 int _isdigit(int c)
@@ -9,4 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+
+#endif
+
