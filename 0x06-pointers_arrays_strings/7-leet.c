2451dbe4d75c15bedd1854a19558fb83a0ff038e
MBrian103 <oduorbrian103@gmail.com>
2022-03-24 14:59:48 -0700

    Mozart composed his music

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,25 @@
 #include "main.h"
 
 /**
- * leet - check the code for Holberton School students.
- * @str: string.
- * Return: string.
+ * leet - function that encode a string
+ * @str:string that will be encoded
+ * Return:returns encoded string
  */
 
 char *leet(char *str)
 
 {
+int index1 = 0, index2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+while (str[++index1])
+{
+for (index2 = 0; index2 <= 7; index2++)
+{
+if (str[index1] == leet[index2] ||
+str[index1] - 32 == leet[index2])
+str[index1] = index2 + '0';
+}
 
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] == 'a' || str[i] == 'A')
-{
-str[i] = '4';
-}
-else if (str[i] == 'e' || str[i] == 'E')
-{
-str[i] = '3';
-}
-else if (str[i] == 'o' || str[i] == 'O')
-{
-str[i] = '0';
-}
-else if (str[i] == 't' || str[i] == 'T')
-{
-str[i] = '7';
-}
-else if (str[i] == 'l' || str[i] == 'L')
-{
-str[i] = '1';
-}
 }
 
 return (str);
