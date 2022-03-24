fd68e3423de96d5a2e925b387a685c1c378a348b
MBrian103 <oduorbrian103@gmail.com>
2022-03-24 14:51:02 -0700

    Mozart composed his music

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,41 @@
 #include "main.h"
 
 /**
- * leet - a function that encodes a string into 1337.
- * @s: an input string to encode
- * Return: An encode string
+ * leet - check the code for Holberton School students.
+ * @str: string.
+ * Return: string.
  */
 
-char *leet(char *s)
+char *leet(char *str)
 
 {
-int i = 0, j;
-char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-while (s[i])
+
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++)
-if (s[i] == str[j])
-s[i] = subs[j];
-i++;
+if (str[i] == 'a' || str[i] == 'A')
+{
+str[i] = '4';
 }
-return (s);
+else if (str[i] == 'e' || str[i] == 'E')
+{
+str[i] = '3';
+}
+else if (str[i] == 'o' || str[i] == 'O')
+{
+str[i] = '0';
+}
+else if (str[i] == 't' || str[i] == 'T')
+{
+str[i] = '7';
+}
+else if (str[i] == 'l' || str[i] == 'L')
+{
+str[i] = '1';
+}
+}
+
+return (str);
 
 }
 
