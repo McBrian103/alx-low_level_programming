6dd3dd84b9c36ce0284922005e003a63c469ff9a
MBrian103 <oduorbrian103@gmail.com>
2022-04-01 14:33:16 -0700

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - program that prints its name
- * @argc: argument count for main
- * @argv: vector to the argument
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
